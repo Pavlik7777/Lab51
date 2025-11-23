23c872b936123b8ce4222b9166074267b9916dff
Pavlik7777 <Pasha20080620@gmail.com>
2025-11-23 14:28:31 +0200

    Добавьте файлы проекта.

---
+++ Lab51/Program.cs
@@ -0,0 +1,7 @@
+﻿//Лабораторная работа №5
+//Строковый тип данных
+//Средний уровень, Вариант 2
+Console.Write("Введите строку:");
+string text = Console.ReadLine();
+int count = text.Split(" ").Length;
+Console.WriteLine($" Количество слов:{count}");
