d50c75e7f53bfe752d1f8358259a6fdab82c9c6c
Pavlik7777 <Pasha20080620@gmail.com>
2025-12-04 21:57:43 +0200

    Program.cs

--- Lab51/Program.cs
+++ Lab51/Program.cs
@@ -3,5 +3,7 @@
 //Средний уровень, Вариант 2
 Console.Write("Введите строку:");
 string text = Console.ReadLine();
-int count = text.Split(" ").Length;
-Console.WriteLine($" Количество слов:{count}");
+char[] signs = new char[] { ' ', ',', '.', '!', '?', ':', ';' };
+string[] words = text.Split(signs);
+int count = words.Length;
+Console.WriteLine($" Количество слов: {count}");
